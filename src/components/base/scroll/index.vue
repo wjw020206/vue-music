dc314c3e925d523f9704e838038404193790b675
CodePencil <jiawei.wang2021@gmail.com>
2025-09-08 15:25:51 +0800

    feat(歌单列表): 完成了界面

---
+++ src/components/base/scroll/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div ref="rootRef"><slot /></div>
+</template>
+
+<script setup>
+import useScroll from './use-scroll'
+import { ref } from 'vue'
+
+const props = defineProps({
+  click: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const rootRef = ref(null)
+useScroll(rootRef, props)
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <Slider v-if="sliders.length" :sliders="sliders" />
+    <Scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <Slider v-if="sliders.length" :sliders="sliders" />
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in albums" class="item" :key="item.id">
+              <div class="icon">
+                <img width="60" height="60" :src="item.pic" v-lazy="item.pic" />
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
@@ -12,12 +34,18 @@
 import { getRecommend } from '@/service/recommend'
 import { onMounted, ref } from 'vue'
 import Slider from '@/components/base/slider/index.vue'
+import Scroll from '@/components/base/scroll/index.vue'
 
+/** 轮播图数据 */
 const sliders = ref([])
+
+/** 歌单列表数据 */
+const albums = ref([])
 
 onMounted(async () => {
   const result = await getRecommend()
   sliders.value = result.sliders
+  albums.value = result.albums
 })
 </script>
 
@@ -28,6 +56,12 @@ onMounted(async () => {
   top: 88px;
   bottom: 0;
   overflow: scroll;
+
+  .recommend-content {
+    height: 100%;
+    overflow: hidden;
+  }
+
   .slider-wrapper {
     position: relative;
     width: 100%;
@@ -40,6 +74,43 @@ onMounted(async () => {
       top: 0;
       width: 100%;
       height: 100%;
+    }
+  }
+  .recommend-list {
+    .list-title {
+      height: 65px;
+      line-height: 65px;
+      text-align: center;
+      font-size: $font-size-medium;
+      color: $color-theme;
+    }
+    .item {
+      display: flex;
+      box-sizing: border-box;
+      align-items: center;
+      padding: 0 20px 20px 20px;
+
+      .icon {
+        flex: 0 0 60px;
+        width: 60px;
+        padding-right: 20px;
+      }
+      .text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        flex: 1;
+        line-height: 20px;
+        overflow: hidden;
+        font-size: $font-size-medium;
+      }
+      .name {
+        margin-bottom: 10px;
+        color: $color-text;
+      }
+      .title {
+        color: $color-text-d;
+      }
     }
   }
 }
