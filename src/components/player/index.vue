6064e495b18f6ceef393b8ebdeb373f5a9fe295e
CodePencil <jiawei.wang2021@gmail.com>
2026-01-07 20:02:38 +0800

    feat: 完成了歌曲播放和暂停的功能

--- src/components/player/index.vue
+++ src/components/player/index.vue
@@ -11,8 +11,27 @@
         <h1 class="title">{{ currentSong.name }}</h1>
         <h2 class="subtitle">{{ currentSong.singer }}</h2>
       </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence" />
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev" />
+          </div>
+          <div @click="togglePlay" class="icon i-center">
+            <i :class="playIcon" />
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next" />
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite" />
+          </div>
+        </div>
+      </div>
     </div>
-    <audio ref="audioRef" />
+    <audio ref="audioRef" @pause="pause" />
   </div>
 </template>
 
@@ -34,6 +53,15 @@ const currentSong = computed(() => {
   return store.getters.currentSong
 })
 
+/** 是否正在播放歌曲 */
+const playing = computed(() => {
+  return store.state.playing
+})
+
+const playIcon = computed(() => {
+  return playing.value ? 'icon-pause' : 'icon-play'
+})
+
 // 监听当前播放歌曲是否发生变化
 watch(currentSong, (newSong) => {
   // 判断歌曲是否合法（没有 id 或者没有播放地址为不合法歌曲数据）
@@ -45,9 +73,26 @@ watch(currentSong, (newSong) => {
   audioEl.play()
 })
 
+// 监听歌曲播放状态的变化
+watch(playing, (newPlaying) => {
+  const audioEl = audioRef.value
+  newPlaying ? audioEl.play() : audioEl.pause()
+})
+
 /** 隐藏播放器全屏播放 */
 function goBack() {
   store.commit('setFullScreen', false)
+}
+
+/** 切换播放状态 */
+function togglePlay() {
+  store.commit('setPlayingState', !playing.value)
+}
+
+/** audio 元素原生歌曲播放暂停事件 */
+// 电脑待机或者睡眠等情况会触发（系统暂停而非用户手动暂停）
+function pause() {
+  store.commit('setPlayingState', false)
 }
 </script>
 
@@ -106,6 +151,41 @@ function goBack() {
         text-align: center;
         font-size: $font-size-medium;
         color: $color-text;
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
       }
     }
   }
