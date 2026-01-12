b1333c818febcc572ca3df3137fdeeea0596c1ad
CodePencil <jiawei.wang2021@gmail.com>
2026-01-13 07:49:44 +0800

    feat: 完成了排行榜页面

--- src/views/top-list.vue
+++ src/views/top-list.vue
@@ -1,7 +1,82 @@
 <template>
-  <div class="top-list">榜单页面</div>
+  <div class="top-list" v-loading="loading">
+    <Scroll class="top-list-content">
+      <ul>
+        <li class="item" v-for="item in topList" :key="item.id">
+          <div class="icon">
+            <img width="100" height="100" v-lazy="item.pic" />
+          </div>
+          <ul class="song-list">
+            <li
+              class="song"
+              v-for="(song, index) in item.songList"
+              :key="song.id"
+            >
+              <span>{{ index + 1 }}. </span>
+              <span>{{ song.songName }}-{{ song.singerName }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </Scroll>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Scroll from '@/components/wrap-scroll'
+import { getTopList } from '@/service/top-list'
+import { onMounted, ref } from 'vue'
 
-<style lang="scss" scoped></style>
+const loading = ref(true)
+/** 排行榜列表 */
+const topList = ref([])
+
+onMounted(async () => {
+  const result = await getTopList()
+  topList.value = result.topList
+  loading.value = false
+})
+</script>
+
+<style lang="scss" scoped>
+.top-list {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .top-list-content {
+    height: 100%;
+    overflow: hidden;
+    .item {
+      display: flex;
+      margin: 0 20px;
+      padding-top: 20px;
+      height: 100px;
+      &:last-child {
+        padding-bottom: 20px;
+      }
+      .icon {
+        flex: 0 0 100px;
+        width: 100px;
+        height: 100px;
+      }
+      .song-list {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 20px;
+        height: 100px;
+        overflow: hidden;
+        background: $color-highlight-background;
+        color: $color-text-d;
+        font-size: $font-size-small;
+        .song {
+          @include no-wrap();
+          line-height: 26px;
+        }
+      }
+    }
+  }
+}
+</style>
