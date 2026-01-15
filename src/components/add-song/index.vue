4a4b77d8c94d4744cfad1632c66a56d5a54c6298
CodePencil <jiawei.wang2021@gmail.com>
2026-01-15 11:01:57 +0800

    fix: 修复了搜索关键词以及首次进入无法滚动最近播放和搜索历史的问题

--- src/components/add-song/index.vue
+++ src/components/add-song/index.vue
@@ -15,12 +15,12 @@
           <Switches :items="['最近播放', '搜索历史']" v-model="currentIndex" />
         </div>
         <div class="list-wrapper" v-show="!query">
-          <Scroll class="list-scroll" v-if="currentIndex === 0">
+          <Scroll class="list-scroll" v-if="currentIndex === 0" ref="scrollRef">
             <div class="list-inner">
               <SongList :songs="playHistory" @select="selectSongBySongList" />
             </div>
           </Scroll>
-          <Scroll class="list-scroll" v-if="currentIndex === 1">
+          <Scroll class="list-scroll" v-if="currentIndex === 1" ref="scrollRef">
             <div class="list-inner">
               <SearchList
                 :searches="searchHistory"
@@ -49,7 +49,7 @@ import Switches from '@/components/base/switches/index.vue'
 import Scroll from '@/components/base/scroll/index.vue'
 import SongList from '@/components/base/song-list/index.vue'
 import SearchList from '@/components/base/search-list/index.vue'
-import { computed, ref } from 'vue'
+import { computed, nextTick, ref, useTemplateRef, watch } from 'vue'
 import { useStore } from 'vuex'
 import useSearchHistory from '@/components/search/use-search-history'
 
@@ -62,6 +62,8 @@ const query = ref('')
 /** 当前切换项的下标 */
 const currentIndex = ref(0)
 
+const scrollRef = useTemplateRef('scrollRef')
+
 /** 搜索历史记录 */
 const searchHistory = computed(() => store.state.searchHistory)
 /** 播放历史记录 */
@@ -69,9 +71,17 @@ const playHistory = computed(() => store.state.playHistory)
 
 const { saveSearch } = useSearchHistory()
 
+watch(query, async () => {
+  await nextTick()
+  refreshScroll()
+})
+
 /** 显示 */
-function show() {
+async function show() {
   visible.value = true
+
+  await nextTick()
+  refreshScroll()
 }
 /** 隐藏 */
 function hide() {
@@ -93,6 +103,9 @@ function selectSongBySuggest(song) {
 /** 添加歌曲进播放列表中 */
 function addSong(song) {
   store.dispatch('addSong', song)
+}
+function refreshScroll() {
+  scrollRef.value.scroll.refresh()
 }
 
 defineExpose({
